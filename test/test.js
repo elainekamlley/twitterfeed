ff3de50d22c5a9eca33195290d09fe10e6db6497
Marco Segreto <marco.segreto@gsa.gov>
2016-10-12 11:50:46 -0700

    Added expectations to test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,23 @@
 var twitterfeed = require('../index.js')
+var expect = require('expect');
 
 var assert = require('assert');
 describe('twitterfeed', function() {
   describe('start()', function() {
     it('start should be a function', function() {
-      assert.equal('function', typeof twitterfeed.start);
+      expect(twitterfeed.start).toBeA('function');
       //mocha checks asserts
     });
   });
 });
 
-//For next session - start here: 
+//For next session - start here:
 
-//Test: There should be a stop 
-//Test: There should be a limit of 10 at a time 
-//Test: The client.stream was called 
+//Test: There should be a stop
+//Test: There should be a limit of 10 at a time
+//Test: The client.stream was called
 //Test: The client.stream was called with a filter of our choosing
 //Test: The client.stream was called with a filter that was passed through a form
 
-//Note: research stubbing and mocking 
-//Note: async testing 
+//Note: research stubbing and mocking
+//Note: async testing
